d1a70ac1db1d9c84a12a69f0a4b8ef258107307d
shineChillar <shine.sl@chillarcards.com>
2024-10-09 13:21:31 +0530

    slot booking added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AppoinmentComponent } from './pages/appoinment/appoinment.component';
 import { AppointmentDetailsComponent } from './pages/appointment-details/appointment-details.component';
+import { SlotBookingComponent } from './pages/slot-booking/slot-booking.component';
 
 export const routes: Routes = [
     {
-        path: "",component:AppoinmentComponent
+        path: "",component:SlotBookingComponent
+    },
+    {
+        path:"appoinment",component:AppoinmentComponent
     },
     {
         path: "appointment-details",component:AppointmentDetailsComponent
--- src/app/pages/slot-booking/slot-booking.component.ts
+++ src/app/pages/slot-booking/slot-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slot-booking',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './slot-booking.component.scss'
 })
 export class SlotBookingComponent {
+  doctors:any =[
+    {
+      id:1,
+      entity:1222,
+      name: 'Dr. Jane Smith',
+      specialization: 'Cardiologist',
+      img: 'doctors.png',
+      experience:6
+    }
+  ]
 
+  constructor(private route: Router){}
+
+  doctorNav(){
+    this.route.navigate(['/appoinment']);
+  }
 }
