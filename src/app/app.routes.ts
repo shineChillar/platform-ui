9eae2110b89247ed89441f6002582c932b6bdce1
shineChillar <shine.sl@chillarcards.com>
2024-10-09 12:33:38 +0530

    route updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AppoinmentComponent } from './pages/appoinment/appoinment.component';
+import { AppointmentDetailsComponent } from './pages/appointment-details/appointment-details.component';
 
 export const routes: Routes = [
     {
         path: "",component:AppoinmentComponent
+    },
+    {
+        path: "appointment-details",component:AppointmentDetailsComponent
     }
 ];
